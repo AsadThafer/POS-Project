d6e27194672cbe35d95aecf57a4ac5c145a03c9d
Asad Asad <asad.gamerz@gmail.com>
2023-02-26 16:18:53 +0200

    NavBar V2.0 updates styling and functionallity

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import authentication from "../../pages/Login/authentication";
 import "./NavBar.css";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 const NavBar = () => {
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => setShowMenu(!showMenu);
 
   const logout = () => {
     localStorage.removeItem("user");
@@ -12,40 +20,36 @@ const NavBar = () => {
     navigate("/");
   };
   return (
-    <header className="navigation">
-      <div className="container">
-        <nav>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <h1 className="logo">
           <Link className="logo" to="/">
             POS-Project
           </Link>
-
-          {authentication() ? (
-            <ul>
-              <li>
-                <Link className="nav-link" to="/CategoriesCRUD">
-                  Categories
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-link" to="/productsMenu">
-                  Products
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-link" to="/orders">
-                  Orders
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-link logout" to="/" onClick={logout}>
-                  Logout
-                </Link>
-              </li>
-            </ul>
-          ) : null}
-        </nav>
+        </h1>
+        <div className="menu-icon" onClick={toggleMenu}>
+          {showMenu ? <MdClose /> : <FaBars />}
+        </div>
+        {authentication() ? (
+          <ul className={`menu ${showMenu ? "show" : ""}`}>
+            <li>
+              <Link to="/CategoriesCRUD">Categories</Link>
+            </li>
+            <li>
+              <Link to="/productsMenu">Products</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+            <li>
+              <Link className="nav-link logout" to="/" onClick={logout}>
+                Logout
+              </Link>
+            </li>
+          </ul>
+        ) : null}
       </div>
-    </header>
+    </nav>
   );
 };
 
