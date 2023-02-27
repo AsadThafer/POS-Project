aa2545a9a65f29f0d78468e9752f02e41a688e71
Asad Asad <asad.gamerz@gmail.com>
2023-02-27 13:07:21 +0200

    tiny bug

--- src/pages/ProductForm/AddProductForm.jsx
+++ src/pages/ProductForm/AddProductForm.jsx
@@ -77,6 +77,7 @@ const AddProductForm = () => {
         }).then((res) => {
           if (res.ok) {
             navigate(`/CategoryDetails/${id}`);
+            window.location.reload();
           }
         });
       }
